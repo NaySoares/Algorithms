b1179518cb3635f45d61fb66f10b254108c733d2
Elienai <elienay.soares07@gmail.com>
2023-03-06 18:21:17 -0300

    feat(C#): add InsertionSort

---
+++ C-Sharp/InsertionSort.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace MyInsertionSort
+{
+    public class ExampleInsertionSort
+    {
+        private void Sort(int[] inputArr)
+        {
+            Console.WriteLine("Unsorted array: ");
+            Array.ForEach(inputArr, Console.Write);
+            Console.WriteLine();
+            int n = inputArr.Length;
+
+            for (int j = 1; j < n; j++)
+            {
+                // current element in unsorted subarray
+                int currentValue = inputArr[j];
+                // last element of sorted subarray
+                int i = j - 1;
+
+                while (i > -1 && currentValue < inputArr[i])
+                {
+                    inputArr[i + 1] = inputArr[i];
+                    i = i - 1;
+                };
+                inputArr[i + 1] = currentValue;
+            };
+        }
+    
+        public static void Main()
+        {
+            int[] arr = { 5,2,4,6,1,3 };
+            var arrUtils = new ExampleInsertionSort();
+            arrUtils.Sort(arr);
+            Console.WriteLine("Sorted array: ");
+            Array.ForEach(arr, Console.Write);
+        }
+    };
+}
